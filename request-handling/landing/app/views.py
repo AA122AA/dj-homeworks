31b5b4a81cd7ee411c79ba138d2d536e472532e7
AA122AA <gromartem2000@gmail.com>
2021-02-04 14:57:53 +0300

    landing done

--- request-handling/landing/app/views.py
+++ request-handling/landing/app/views.py
@@ -12,6 +12,10 @@ counter_click = Counter()
 
 def index(request):
     # Реализуйте логику подсчета количества переходов с лендига по GET параметру from-landing
+    if request.GET.get("from-landing") == "test":
+        counter_click["test"] += 1
+    elif request.GET.get("from-landing") == "original":
+        counter_click["original"] += 1
     return render(request, 'index.html')
 
 
@@ -20,13 +24,18 @@ def landing(request):
     # в зависимости от GET параметра ab-test-arg
     # который может принимать значения original и test
     # Так же реализуйте логику подсчета количества показов
-    return render(request, 'landing.html')
+    if request.GET.get("ab-test-arg") == "test":
+        counter_show["test"] += 1
+        return render(request, 'landing_alternate.html')
+    else:
+        counter_show["original"] += 1
+        return render(request, 'landing.html')
 
 
 def stats(request):
     # Реализуйте логику подсчета отношения количества переходов к количеству показов страницы
     # Для вывода результат передайте в следующем формате:
     return render(request, 'stats.html', context={
-        'test_conversion': 0.5,
-        'original_conversion': 0.4,
+        'test_conversion': f"{float(counter_click.get('test', 1))/float(counter_show.get('test', 1)):.1f}",
+        'original_conversion': f"{float(counter_click.get('original', 1))/float(counter_show.get('original', 1)):.1f}",
     })
