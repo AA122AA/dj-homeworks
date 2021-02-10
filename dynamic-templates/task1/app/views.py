1c856db4a92bdeb554e5b17ff24ac08d94db7244
AA122AA <gromartem2000@gmail.com>
2021-02-10 16:48:37 +0300

    done task1

--- dynamic-templates/task1/app/views.py
+++ dynamic-templates/task1/app/views.py
@@ -7,36 +7,12 @@ def createList()->list:
     with open("inflation_russia.csv", "r") as f:
         reader = csv.DictReader(f, delimiter = ";")    
         for row in reader:
-            val = list(row.values())
-            for v in range(len(val)):
-                if val[v] == "":
-                    val[v] = "0"
-            print(list(row.values()))
-            # Убрать это и переделать на мердж словарей нормальный с проверкой на пустые строки
-            data = {}
-            data["Year"] = row.get("Год")
-            data["Jan"] = float(row.get("Янв", "-"))
-            data["Feb"] = float(row.get("Фев", "-"))
-            data["Mar"] = float(row.get("Мар", "-"))
-            data["Apr"] = float(row.get("Апр", "-"))
-            data["May"] = float(row.get("Май", "-"))
-            data["Jun"] = float(row.get("Июн", "-"))
-            data["Jul"] = float(row.get("Июл", "-"))
-            data["Aug"] = float(row.get("Авг", "-"))
-            data["Sep"] = float(row.get("Сен", "-"))
-            data["Oct"] = float(row.get("Окт", "-"))
-            data["Now"] = float(row.get("Ноя", "-"))
-            data["Dec"] = float(row.get("Дек", "-"))
-            data["Sum"] = float(row.get("Суммарная", "-"))
-            dataList.append(data)
+            dataList.append(row)
     return dataList
  
 
 def inflation_view(request):
     template_name = 'inflation.html'
-    print(*createList(), sep="\n")
-
-       # чтение csv-файла и заполнение контекста
     context = {
         "data": createList(), 
         "headers": list(createList()[0].keys())
