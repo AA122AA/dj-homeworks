047c01433007ec59d568da4df44936841d84e0bf
AA122AA <gromartem2000@gmail.com>
2021-02-14 12:10:29 +0300

    start

--- databases/work_with_database/main/settings.py
+++ databases/work_with_database/main/settings.py
@@ -51,9 +51,10 @@ MIDDLEWARE = [
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'netology_import_phones',
-        'HOST': '127.0.0.1',
-        'PORT': '5432',
+        'NAME': 'postappdb',
+        'USER': 'artem',
+        'PASSWORD': '1234',
+        'HOST': 'localhost',
     }
 }
 
