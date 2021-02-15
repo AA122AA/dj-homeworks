1f13313c13f42d00506934e042601f4041ea7768
AA122AA <gromartem2000@gmail.com>
2021-02-15 15:27:53 +0300

    models update

--- databases/phone_comparison/phones/models.py
+++ databases/phone_comparison/phones/models.py
@@ -4,9 +4,14 @@ from django.db import models
 class Phone(models.Model):
     name = models.CharField(max_length=50)
     price = models.IntegerField()
-    image = models.URLField()
     release_date = models.DateField()
     lte_exists = models.BooleanField()
+    bluetooth_exists = models.BooleanField()
+    os = models.CharField(max_length=15)
+    megapixels = models.IntegerField()
+    ram = models.IntegerField()
+    rom = models.IntegerField()
+    screen = models.CharField(max_length=40)
     slug = models.SlugField()
 
     def __str__(self):
