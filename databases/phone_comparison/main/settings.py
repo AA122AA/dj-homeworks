3e81fafe76e3cc15f51919da5cd5032916cff155
AA122AA <gromartem2000@gmail.com>
2021-02-15 13:25:08 +0300

    add ignore, db connection, start model

--- databases/phone_comparison/main/settings.py
+++ databases/phone_comparison/main/settings.py
@@ -50,9 +50,10 @@ MIDDLEWARE = [
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'netology_phone_comparison',
-        'HOST': '127.0.0.1',
-        'PORT': '5432',
+        'NAME': 'postgresapp',
+        'USER': 'artem',
+        'PASSWORD': '1234',
+        'HOST': '192.168.0.30',
     }
 }
 
--- databases/phone_comparison/phones/models.py
+++ databases/phone_comparison/phones/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+class Phone(models.Model):
+    name = models.CharField(max_length=50)
+    price = models.IntegerField()
+    image = models.URLField()
+    release_date = models.DateField()
+    lte_exists = models.BooleanField()
+    slug = models.SlugField()
+
+    def __str__(self):
+        return self.name
+    
