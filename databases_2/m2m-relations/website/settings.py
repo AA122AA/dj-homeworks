0e49aaa1e65549a67571a0b85f2ad3004a1e3c48
AA122AA <gromartem2000@gmail.com>
2021-02-23 17:51:13 +0300

    add .env

--- databases_2/m2m-relations/website/settings.py
+++ databases_2/m2m-relations/website/settings.py
@@ -75,10 +75,10 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'm2m_relations',
-        'USER': 'artem',
-        'PASSWORD': '1234',
-        'HOST': '188.242.131.186',
-        'PORT': '5122',
+        'USER': os.getenv("USER"),
+        'PASSWORD': os.getenv("PASSWORD"),
+        'HOST': os.getenv("IP"),
+        'PORT': os.getenv("PORT"),
     }
 }
 
