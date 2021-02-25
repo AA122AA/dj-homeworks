7f641a1c9a19ea395762c03ac0f1318bacf67e7f
AA122AA <gromartem2000@gmail.com>
2021-02-25 21:11:06 +0300

    edit sort func

--- databases/work_with_database/phones/views.py
+++ databases/work_with_database/phones/views.py
@@ -4,38 +4,28 @@ from phones.models import Phone
 from django.http.request import HttpRequest
 
 
-def sorting(sort:str, phones:BaseManager):
+def sorting(sort:str):
     s_list = []
     if sort == "name":
-        s_list = sorted(
-            list(phones), 
-            key=lambda phone: phone.name
-            )
+        s_list = list(Phone.objects.order_by("name"))
     elif sort == "min_price":
-        s_list = sorted(
-            list(phones), 
-            key=lambda phone: phone.price,
-            reverse=True
-            )
+        s_list = list(Phone.objects.order_by("-price"))
     elif sort == "max_price":
-       s_list = sorted(
-            list(phones), 
-            key=lambda phone: phone.price,
-            reverse=False
-            )
+        s_list = list(Phone.objects.order_by("price"))
     else:
-        s_list = phones
+        s_list = Phone.objects.all()
     return s_list
 
 
 def show_catalog(request:HttpRequest):
     template = 'catalog.html'
     sort = request.GET.get('sort')
-    phones = Phone.objects.all()
-    context = {"phones": list(phones)}
     if sort:
-        s_list = sorting(sort, phones)
+        s_list = sorting(sort)
         context = {"phones": list(s_list)}
+    else:
+        phones = Phone.objects.all()
+        context = {"phones": list(phones)}
     
     return render(request, template, context)
 
