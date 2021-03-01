b8cf6999a8ebe314b9649cb8db95b07b14594f78
AA122AA <gromartem2000@gmail.com>
2021-03-01 16:38:34 +0300

    add .gitignore edit settings

--- drf-auth-and-validation/api_with_restrictions/api_with_restrictions/settings.py
+++ drf-auth-and-validation/api_with_restrictions/api_with_restrictions/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+from .config import *
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -89,8 +90,10 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'netology_classified_ads',
-        'HOST': '127.0.0.1',
-        'PORT': '5432',
+        'USER': USER,
+        'PASSWORD': PASSWORD,
+        'HOST': HOST,
+        'PORT': PORT,
     }
 }
 
